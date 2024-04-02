6638818a3110bb275f8a1763513ea1232ede0b67
Ángel Ortega <codenode.ortega@gmail.com>
2024-04-01 18:52:12 -0600

    add token model of data

---
+++ src/main/java/com/ASSESSMENT/Meeting/config/StructureToken/UserToken.java
@@ -0,0 +1,40 @@
+package com.ASSESSMENT.Meeting.config.StructureToken;
+
+public class UserToken {
+    private String Token;
+    private String Email;
+    private String Username;
+    private String Id;
+
+    public String getToken() {
+        return Token;
+    }
+
+    public void setToken(String token) {
+        Token = token;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+}
