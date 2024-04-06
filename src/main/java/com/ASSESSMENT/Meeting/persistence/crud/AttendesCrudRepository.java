8fe0a79d25de571e774d75544a7a9184f5722d3c
Ángel Ortega <codenode.ortega@gmail.com>
2024-04-06 05:02:29 -0600

    Add petitions Sql in AttendesCrudRepository

--- src/main/java/com/ASSESSMENT/Meeting/persistence/crud/AttendesCrudRepository.java
+++ src/main/java/com/ASSESSMENT/Meeting/persistence/crud/AttendesCrudRepository.java
@@ -1,8 +1,15 @@
 package com.ASSESSMENT.Meeting.persistence.crud;
 
 import com.ASSESSMENT.Meeting.persistence.entity.Attendees;
+import com.ASSESSMENT.Meeting.persistence.entity.Session;
+import com.ASSESSMENT.Meeting.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AttendesCrudRepository extends JpaRepository<Attendees, Long> {
+    @Query(value = "SELECT * FROM attendees WHERE session_id = :sessionId AND user_id = :userId", nativeQuery = true)
+    Attendees findBySessionIdAndUserId(@Param("sessionId") Long sessionId, @Param("userId") Long userId);
+
 }
